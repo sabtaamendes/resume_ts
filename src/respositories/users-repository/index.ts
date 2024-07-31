13fb4e13d17c8ead577e14bdb1fe13dadf30e762
sabtaamendes <gfacilittisabta@gmail.com>
2024-07-31 11:16:10 -0400

    fix: ci yaml

--- src/respositories/users-repository/index.ts
+++ src/respositories/users-repository/index.ts
@@ -1,19 +1,7 @@
 import { prisma } from "../../configs/database";
-import { Prisma } from "@prisma/client";
 
 async function findByEmail(email: string) {
-  // const params: Prisma.usersFindUniqueArgs = {
-  //   where: {
-  //     email,
-  //   },
-  // };
-
-  // if (select) {
-  //   params.select = select;
-  // }
-
-  // return await prisma.users.findUnique(params);
-
+ 
   return await prisma.users.findUnique({
     where: {
       email,
